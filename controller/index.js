349dee57743869120dd3a8ce9738435e235b9358
houfujian <ApeLiftingIron@163.com>
2018-08-19 22:44:48 +0800

    收集ua和ip完成

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,14 @@
 const Router = require('koa-router')
 const router = new Router()
-
+const parser = require('ua-parser-js');
+const internalIp = require('internal-ip');
 router.get('/', (ctx, next) => {
-  ctx.body = `<h1>${ctx.request.header['user-agent']}</h1>`
+  // ctx.body = `<h1>${ctx.request.header['user-agent']}</h1>`
+  let ua = parser(ctx.request.header['user-agent']);
+  let newUa = Object.assign({}, ua, {ip: internalIp.v4.sync()})
+  // write the result as response
+  ctx.body = JSON.stringify(newUa, null, '  ');
+  // console.log(internalIp.v4.sync())
 })
 
 router.get('/useragent', (ctx, next) => {
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const KOA = require('koa')
 const routes = require('../controller')
 const PORT = 5000
 const app = new KOA()
+const serve = require('koa-static')
+const path = require('path')
 // const { connect, initSchemas } = require('../database/init');
 
 app.use(routes)
-
+app.use(serve(path.join(__dirname, '../page'), {extensions: ['html']}));
+// console.log(path.join(__dirname, '../page'))
 app.listen(PORT)
 console.log('server listening at port:' + PORT + '!');
